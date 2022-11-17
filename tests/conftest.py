146cd71281abe7edded9c3bf465e3aab7633a5a6
emdee <emdee>
2022-11-17 08:51:30 +0000

    add .github/workflows/test.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
-# © 2018 Gerard Marull-Paretas <gerard@teslabs.com>
-# © 2014 Mark Harviston <mark.harviston@gmail.com>
-# © 2014 Arve Knudsen <arve.knudsen@gmail.com>
+# -*- mode: python; indent-tabs-mode: nil; py-indent-offset: 4; coding: utf-8 -*-
+
+# (c) 2018 Gerard Marull-Paretas <gerard@teslabs.com>
+# (c) 2014 Mark Harviston <mark.harviston@gmail.com>
+# (c) 2014 Arve Knudsen <arve.knudsen@gmail.com>
 # BSD License
 
 # phantompy test - just test qasync for now
@@ -9,17 +11,9 @@ import os
 import logging
 from pytest import fixture
 
-
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
 )
-
-
-if os.name == "nt":
-    collect_ignore = ["qasync/_unix.py"]
-else:
-    collect_ignore = ["qasync/_windows.py"]
-
 
 @fixture(scope="session")
 def application():
